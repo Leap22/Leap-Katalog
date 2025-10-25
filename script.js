c6c5399fd6122a908c4893b0ddd782c9be7f7069
Leap22 <pirinsiagian2605@gmail.com>
2025-10-25 19:32:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function tampilkanProduk(produkArray) {
             </div>
             <p><strong>KODE SKU:</strong> <span class="kode-sku">${produk.kode_sku}</span></p>
                 
-                <a href="https://wa.me/[85512345678]?text=Halo%2C%20saya%20tertarik%20dengan%20produk%20${produk.kode_sku}%20yang%20saya%20lihat%20di%20website." target="_blank" class="tombol-wa">
+                <a href="https://wa.me/85589640025?text=Halo%2C%20saya%20tertarik%20dengan%20produk%20${produk.kode_sku}%20yang%20saya%20lihat%20di%20website." target="_blank" class="tombol-wa">
                     Pesan via WhatsApp
                 </a>
             </div> `;
@@ -47,5 +47,4 @@ function tampilkanProduk(produkArray) {
 }
 
 // Panggil fungsi utama saat halaman dimuat
-
 ambilDataProduk();
